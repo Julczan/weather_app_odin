14f9dec5dc81742fe911478a3896a5b3d39c2302
Julczan <julek111222h@gmail.com>
2025-08-24 16:30:39 +0200

    weather api call

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
 import "./styles.css";
-
-// async function getWeatherData() {
-//   try {
-//     const response = await fetch(
-//       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/krak%C3%B3w?unitGroup=us&key=YCKCFFZDYCQ6HNKX46VCM7SL6&contentType=json",
-//       {
-//         mode: "cors",
-//       }
-//     );
-//     const json = await response.json();
-//     console.log(json);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+import { getWeatherData } from "./weatherAPI";
---
+++ src/weatherAPI.js
@@ -0,0 +1,22 @@
+const API_key = "YCKCFFZDYCQ6HNKX46VCM7SL6";
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_key}`,
+      {
+        mode: "cors",
+      }
+    );
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export { getWeatherData };
