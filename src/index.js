9cd1b7e30ccda8ba60969b3db9ce42139b7edb92
Julczan <julek111222h@gmail.com>
2025-08-24 20:31:46 +0200

    get icon dynamically

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import "./styles.css";
 import { currConditions } from "./processData";
 
-import sunnyImage from "./assets/SVG/1st Set - Color/clear-day.svg";
-
 const input = document.querySelector("#location");
 const button = document.querySelector("#search");
+const container = document.querySelector(".container");
 
 button.addEventListener("click", () => {
-  const currData = currConditions(input.value);
-
-  currData.then((result) => console.log(result));
+  getIconForData();
 });
+
+async function getIconForData() {
+  const currData = await currConditions(input.value);
+  let currIcon = currData.icon;
+  const icon = await import(`./assets/PNG/2nd Set - Color/${currIcon}.png`);
+  const iconSrc = icon.default;
+  const image = document.createElement("img");
+
+  image.src = iconSrc;
+
+  container.appendChild(image);
+}
