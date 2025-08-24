77e961d2e44c1ff3c0bd6ebff427310174a3b718
Julczan <julek111222h@gmail.com>
2025-08-24 16:09:33 +0200

    template repo test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles.css";
-console.log("conf");
+
+// async function getWeatherData() {
+//   try {
+//     const response = await fetch(
+//       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/krak%C3%B3w?unitGroup=us&key=YCKCFFZDYCQ6HNKX46VCM7SL6&contentType=json",
+//       {
+//         mode: "cors",
+//       }
+//     );
+//     const json = await response.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
