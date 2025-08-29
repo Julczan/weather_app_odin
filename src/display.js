47511bb9e82648a3174718dc0350ef4413f6bc89
Julczan <julek111222h@gmail.com>
2025-08-29 14:30:59 +0200

    adding loading spinner

--- src/display.js
+++ src/display.js
@@ -1,9 +1,19 @@
 import { getDataAndIcon, getDailyDataAndIcon } from ".";
 
+function loadingSpinner(display) {
+  display.textContent = "";
+  const spinner = document.createElement("div");
+  spinner.className = "loader";
+
+  display.appendChild(spinner);
+}
+
 async function displayTodayData() {
+  const display = document.querySelector(".display");
+
+  loadingSpinner(display);
   const data = await getDataAndIcon();
 
-  const display = document.querySelector(".display");
   const container = document.createElement("div");
   const address = document.createElement("div");
   const icon = document.createElement("img");
@@ -39,9 +49,12 @@ async function displayTodayData() {
 }
 
 async function displayDailyData() {
+  const display = document.querySelector(".display");
+
+  loadingSpinner(display);
+
   const data = await getDailyDataAndIcon();
 
-  const display = document.querySelector(".display");
   const container = document.createElement("div");
   const table = document.createElement("div");
   const dateText = document.createElement("div");
